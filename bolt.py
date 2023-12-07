21192318461227f03afe38b86925d65e69f96ce3
karlisk4 <karlis.skirmanis@gmail.com>
2023-12-07 15:58:31 +0200

    removed whitespaces

--- bolt.py
+++ bolt.py
@@ -1,7 +1,6 @@
 import requests
 import pandas
 import numpy as np
-
 prev_date = ""
 
 def pdf_create(bolt_list):
@@ -12,19 +11,15 @@ def pdf_create(bolt_list):
             response = requests.get(elem[0])
         except:
             print("Could not request file")
-
         if response.status_code == 200:
             date = elem[1]
             if date[:11] == prev_date:
                     i=i+1
             else:
                     i=1            
-
             new_name = str(date[2:4]) + str(date[5:7]) + str(date[8:10]) + "_bolt" + str(i) + ".pdf"
-
             with open(new_name, "wb") as raw_pdf:
                 raw_pdf.write(response.content)
-                
             prev_date=date[:11]
 
 def read_data_csv(file):
